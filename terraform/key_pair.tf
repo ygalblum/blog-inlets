49ca1fc1a594e644e7a15b241a2b6eff762d69b7
Ygal Blum <ygal.blum@gmail.com>
2023-08-17 17:24:11 +0300

    TF - chmod the downloaded private key

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -8,6 +8,6 @@ resource "aws_key_pair" "inlets_key_pair" {
   public_key = tls_private_key.inlets_key.public_key_openssh
 
   provisioner "local-exec"{
-    command = "echo '${tls_private_key.inlets_key.private_key_pem}' > ./${var.key_pair_name}.pem"
+    command = "echo '${tls_private_key.inlets_key.private_key_pem}' > ./${var.key_pair_name}.pem && chmod 600 ./${var.key_pair_name}.pem"
   }
 }
