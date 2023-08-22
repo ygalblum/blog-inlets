587db6a490d46dde46c5db3aa1e828a6a5e927ec
Ygal Blum <ygal.blum@gmail.com>
2023-08-22 17:50:24 +0300

    TF - create private key using local resource

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -6,8 +6,10 @@ resource "tls_private_key" "inlets_key" {
 resource "aws_key_pair" "inlets_key_pair" {
   key_name   = var.key_pair_name
   public_key = tls_private_key.inlets_key.public_key_openssh
+}
 
-  provisioner "local-exec"{
-    command = "echo '${tls_private_key.inlets_key.private_key_pem}' > ./${var.key_pair_name}.pem && chmod 600 ./${var.key_pair_name}.pem"
-  }
+resource "local_sensitive_file" "ssh_key" {
+  content = "${tls_private_key.inlets_key.private_key_pem}"
+  filename = "${path.cwd}/${var.key_pair_name}.pem"
+  file_permission = "0600"
 }
