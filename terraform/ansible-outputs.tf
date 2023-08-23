7d682c9306c7e8373610e001de90ad4167775337
Ygal Blum <ygal.blum@gmail.com>
2023-08-23 13:47:43 +0300

    Move ansible connection variable to global vars

--- terraform/ansible-outputs.tf
+++ terraform/ansible-outputs.tf
@@ -2,24 +2,21 @@ resource "local_file" "ansible_inventory" {
   content = yamlencode(
     {
       all: {
+        vars: {
+          ansible_user: "ec2-user",
+          ansible_ssh_private_key_file: "${local_sensitive_file.ssh_key.filename}",
+          ansible_ssh_common_args: "-o StrictHostKeyChecking=no"
+        }
         hosts: {},
         children: {
           inlets_server: {
             hosts: {
-              "${aws_instance.inlets_server.public_ip}": {
-                ansible_user: "ec2-user",
-                ansible_ssh_private_key_file: "${local_sensitive_file.ssh_key.filename}",
-                ansible_ssh_common_args: "-o StrictHostKeyChecking=no"
-              }
+              "${aws_instance.inlets_server.public_ip}": {}
             }
           },
           inlets_client: {
             hosts: {
-              "${aws_instance.inlets_client.public_ip}": {
-                ansible_user: "ec2-user",
-                ansible_ssh_private_key_file: "${local_sensitive_file.ssh_key.filename}",
-                ansible_ssh_common_args: "-o StrictHostKeyChecking=no"
-              }
+              "${aws_instance.inlets_client.public_ip}": {}
             }
           }
         }
